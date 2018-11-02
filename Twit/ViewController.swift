b1530a8967d8b4e4cb847d7dc033d8878749d31f
Henry Allsuch <henry@agilepixel.io>
2018-11-02 21:07:52 +0000

    • adding console

--- Twit/ViewController.swift
+++ Twit/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var console: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        logToView("")
+        logToView("View Did Load")
+       
     }
 
-
+    func logToView(_ message :String, clearAllText: Bool? = nil , newLine: Bool? = true){
+        
+        //always update UI in main thread!
+        DispatchQueue.main.async {
+            
+            
+            self.console.text.append(message);
+            if(newLine != nil){
+                self.console.text.append("\n");
+            }
+            
+            let range = NSMakeRange(self.console.text.count - 1, 0)
+            self.console.scrollRangeToVisible(range)
+            
+            print(message)
+            
+        }
+    }
 }
 
